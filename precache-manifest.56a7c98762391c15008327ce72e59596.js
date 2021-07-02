4d4824b22d6aaf14f1bdc0f178e452f06fa3c6db
macharry89 <ok.star@outlook.com>
2021-07-03 01:47:02 +0800

    Updates

---
+++ precache-manifest.56a7c98762391c15008327ce72e59596.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "34d76b49f7b781de357f",
+    "url": "./static/css/main.7a506f7b.chunk.css"
+  },
+  {
+    "revision": "34d76b49f7b781de357f",
+    "url": "./static/js/main.34d76b49.chunk.js"
+  },
+  {
+    "revision": "373f51fae2d42dae1196",
+    "url": "./static/css/1.7a16ce7a.chunk.css"
+  },
+  {
+    "revision": "373f51fae2d42dae1196",
+    "url": "./static/js/1.373f51fa.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "023762b6aec2a2249b8fdfb638f00ef3",
+    "url": "./static/media/metamask.023762b6.png"
+  },
+  {
+    "revision": "66e8d9fdab96d390082480cb0f38bcfb",
+    "url": "./index.html"
+  }
+];
